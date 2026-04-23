210adfcbbc19e1f47e35b20d3b38937241463ec1
rlnt <16513358+rlnt@users.noreply.github.com>
2026-04-23 14:06:15 +0200

    fix minimum MDG version not being resolved

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,7 @@ tasks {
 buildConfig {
     buildConfigField("String", "VERSION", "\"${project.version}\"")
     buildConfigField("String", "MINIMUM_GRADLE_VERSION", "\"$minimumGradleVersion\"")
-    buildConfigField("String", "MINIMUM_MDG_VERSION", "\"${libs.versions.moddevgradle}\"")
+    buildConfigField("String", "MINIMUM_MDG_VERSION", "\"${libs.versions.moddevgradle.get()}\"")
 }
 
 gradlePlugin {
